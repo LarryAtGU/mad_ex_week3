024b00f0959e174bc680be842e60e5ac52777573
Larry Wen <l.wen@griffith.edu.au>
2024-03-01 21:41:18 +1000

    setup the environment

---
+++ ex.test.js
@@ -0,0 +1,13 @@
+/**
+ * @file ex.test.js
+ * @description Test suite for Week 3 exercises of the Mobile Application Development
+ *              course. This file contains test cases that validate the correctness of
+ *              the code written in the exercises. The tests are designed to assess
+ *              the functionality of the implemented solutions against specified
+ *              requirements and scenarios.
+ * @author Larry Wen
+ * @created [01/03/2024]
+ *
+ * NOTE: This file is not intended to be modified by the students. Alterations to this
+ * test suite may result in inaccurate assessments of the exercise solutions.
+ */
